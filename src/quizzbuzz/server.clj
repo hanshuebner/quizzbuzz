4d4a7840c731209f6bfead3da49c41d918679ea0
Hans Huebner <hans.huebner@lambdawerk.com>
2016-10-11 22:42:06 +0200

    Datenbank, Kategorien...

--- src/quizzbuzz/server.clj
+++ src/quizzbuzz/server.clj
@@ -7,7 +7,9 @@
             [ring.util.response :as response]
             [ring.adapter.jetty :refer [run-jetty]]
             [compojure.core :refer :all]
-            [compojure.route :as route]))
+            [compojure.route :as route]
+            [me.raynes.fs :as fs]
+            [clojure.string :as string]))
 
 (defn ensure-string [thing]
   (if (float? thing)
@@ -23,70 +25,105 @@
          :answer-incorrect-3 (ensure-string answer-incorrect-3)
          :level (int level)}))
 
-(def get-questions
-  (memoize (fn [resource-name]
-             (->> (spreadsheet/load-workbook-from-resource resource-name)
-                  (spreadsheet/select-sheet "Tabelle1")
-                  (spreadsheet/select-columns {:A :question
-                                               :B :answer-correct
-                                               :C :answer-incorrect-1
-                                               :D :answer-incorrect-2
-                                               :E :answer-incorrect-3
-                                               :F :level
-                                               :G :category})
-                  rest
-                  (map normalize-question)))))
+(defn load-questions-from-excel [resource-name]
+  (->> (spreadsheet/load-workbook-from-resource resource-name)
+       (spreadsheet/select-sheet "Tabelle1")
+       (spreadsheet/select-columns {:A :question
+                                    :B :answer-correct
+                                    :C :answer-incorrect-1
+                                    :D :answer-incorrect-2
+                                    :E :answer-incorrect-3
+                                    :F :level
+                                    :G :category})
+       rest
+       (map normalize-question)))
 
-(defn get-question
-  ([resource-name]
-   (get-question resource-name {}))
-  ([resource-name {:keys [level category]}]
-   (let [wanted (fn [question]
-                  (and (or (not level)
-                           (= level (:level question)))
-                       (or (not category)
-                           (= category (:category question)))))]
-     (->> (get-questions resource-name)
-          (filter wanted)
-          seq
-          rand-nth))))
+(defn catego-and-randomize [questions]
+  (->> questions
+       (group-by :category)
+       (map (fn [[category questions]]
+              [category (shuffle questions)]))
+       (into {})))
+
+(defn questions-upto-level [questions max-level]
+  (filter #(<= (:level %) max-level) questions))
+
+(defn categories-with-questions [database question-count max-level]
+  (filter (fn [category]
+            (>= (count (questions-upto-level (database category) max-level)) question-count))
+          (keys database)))
+
+(defn get-questions [database category question-count max-level]
+  (let [questions-chosen (take question-count (questions-upto-level (database category) max-level))]
+    [questions-chosen
+     (update database category (partial remove (set questions-chosen)))]))
 
 (defn statistics [resource-name]
-  (->> (get-questions resource-name)
+  (->> (load-questions-from-excel resource-name)
        (group-by :category)
        (map (fn [[category questions]]
               [category (frequencies (map :level questions))]))
        (into {})))
 
-(defn known-categories [resource-name]
-  (-> (statistics resource-name)
-      keys
-      set))
+(defn known-categories [database]
+  (set (keys database)))
 
-(defn get-question-handler [{:keys [params config]}]
+(defn parse-params [{:keys [question-count max-level] :as params}]
+  (assoc params
+         :question-count (if question-count
+                           (Integer/parseInt question-count)
+                           10)
+         :max-level (if max-level
+                      (Integer/parseInt max-level)
+                      10)))
+
+(defn get-questions-handler [{:keys [params config database]}]
   (let [{:keys [questions-resource-name]} config
-        {:keys [level category]} params
-        level (and level (Integer/parseInt level))]
+        {:keys [max-level category question-count]} (parse-params params)]
     (cond
-      (and level
-           (not (<= 1 level 3)))
-      (-> (response/response "invalid level")
+      (not (<= 1 max-level 10))
+      (-> (response/response "invalid max-level")
           (response/status 400))
 
-      (and category
-           (not (get (known-categories questions-resource-name) category)))
+      (not (get (known-categories database) category))
       (-> (response/response "invalid category")
           (response/status 400))
 
       :otherwise
-      (if-let [question (get-question questions-resource-name
-                                      {:level level
-                                       :category category})]
-        (response/response question)
-        (response/not-found "no matching question found")))))
+      (let [[questions database] (get-questions database
+                                                category
+                                                question-count
+                                                max-level)]
+        (if (seq questions)
+          (assoc (response/response questions)
+                 :database database)
+          (response/not-found "no matching questions found"))))))
+
+(defn get-categories-handler [{:keys [database params]}]
+  (let [{:keys [max-level category question-count]} (parse-params params)]
+    (response/response (categories-with-questions database question-count max-level))))
+
+(defn load-database [database-file]
+  (if (fs/exists? database-file)
+    (read-string (slurp database-file))
+    (-> (load-questions-from-excel "FragenAuswahl.xlsx")
+        catego-and-randomize)))
+
+(defn wrap-database [handler]
+  (fn [{:keys [headers] :as request}]
+    (if-let [client-id (get headers "x-client-id")]
+      (let [database-file (fs/file "database" (string/replace client-id #"(?i)[^a-z0-9-]" ""))
+            response (handler (assoc request
+                                     :database (load-database database-file)))]
+        (when-let [database (:database response)]
+          (spit database-file (pr-str database)))
+        (dissoc response :database))
+      (-> (response/response "missing X-Client-ID header")
+          (response/status 400)))))
 
 (defroutes app
-  (POST "/question" [] get-question-handler)
+  (POST "/questions" [] get-questions-handler)
+  (GET "/categories" [] get-categories-handler)
   (route/not-found "page not found"))
 
 (defonce server (atom nil))
@@ -102,6 +139,6 @@
                                  wrap-json-response
                                  wrap-keyword-params
                                  wrap-params
-                                 wrap-config)
+                                 wrap-database)
                              {:port port
                               :join? false}))))
